757aac7c0f792173b8b36fd9679a17525ec77d2f
Shahdidul <theshahidkhan.2004@gmail.com>
2025-12-01 03:52:14 +0600

    added rate limits for api's

--- backend/eat_wise/eat_wise/settings.py
+++ backend/eat_wise/eat_wise/settings.py
@@ -64,12 +64,14 @@ CORS_ALLOW_ALL_ORIGINS = True  # Change this to specific origins in production
 CSRF_TRUSTED_ORIGINS = ["https://*.vercel.app"]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 SIMPLE_JWT = {
